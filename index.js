074dee3db606c7e708f5c3bd7cf9fb18e924dfff
Levan Buchukuri <Levanbuchukuri1993@gmail.com>
2019-01-08 16:54:47 +0600

    changes in generator

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 var random = require('true-random');
 
-
 const fs = require("fs");
 const path = require("path");
 
@@ -9,24 +8,25 @@ let bot = new Discord.Client();
 
 const token = process.env.BOT_TOKEN;
 
-bot.on('ready',()=>{
+bot.on('ready', () => {
   console.log("Alright, alright, alright")
 });
 
 bot.on('message', msg => {
   if (msg.content === 'dices, dude') {
 
-    let gen = new random.true_rand(); //Create a number generator, this function accepts a cache_size, min_cache and a callback parameter
-    let integer = gen.integer() //returns a integer between 0 and 1, you can pass diffrent min and max values with integer(min, max)
-    let integer_array = gen.integers(0, 9, 8);
+    var gen = new random.true_rand(10, 5, function (gen) {
+      let integer_array = gen.integers(0, 9, 8);
 
-    var numbers = "";
-    
-    for(let i =0; i < integer_array.length; i++) {
-      numbers+=integer_array[i];
-    }
+      var numbers = "";
 
-    msg.reply(numbers);
+      for (let i = 0; i < integer_array.length; i++) {
+        numbers += integer_array[i];
+      }
+
+      msg.reply(numbers);
+    });
+
   }
 });
 
