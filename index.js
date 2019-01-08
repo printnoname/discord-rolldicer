c3070c699900ef57aacc13cc10609b0c9e1b1618
printnoname <printnoname@gmail.com>
2019-01-08 22:27:22 +0600

    changed lib for generating random values

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const Discord = require('discord.js');
-var random = require('true-random');
-
-const fs = require("fs");
-const path = require("path");
+const random = require('random');
 
 let bot = new Discord.Client();
 
-const token = process.env.BOT_TOKEN;
+//const token = process.env.BOT_TOKEN;
+const token = "NTMwMDMxODEyODE5NTUwMjA4.DxZdBg.0W9J7lTsUkD5o-P6BJtyFoSeHzg";
 
 bot.on('ready', () => {
   console.log("Alright, alright, alright")
@@ -15,18 +13,13 @@ bot.on('ready', () => {
 bot.on('message', msg => {
   if (msg.content === 'dices, dude') {
 
-    var gen = new random.true_rand(10, 5, function (gen) {
-      let integer_array = gen.integers(0, 9, 8);
+      let numbers = "";
 
-      var numbers = "";
-
-      for (let i = 0; i < integer_array.length; i++) {
-        numbers += integer_array[i];
+      for (let i = 0; i < 8; i++) {
+        numbers += random.int(0,9);
       }
 
       msg.reply(numbers);
-    });
-
   }
 });
 
