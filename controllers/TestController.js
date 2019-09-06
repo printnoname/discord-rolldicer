dd269539681dd0aa302b77d45d7318eef8bdb7ad
printnoname <printnoname@gmail.com>
2019-09-06 15:47:48 +0600

    improved mock tester

--- controllers/TestController.js
+++ controllers/TestController.js
@@ -6,12 +6,6 @@ const axios = require('axios');
 module.exports.sendData = async function(req,res){
    var sampleData = JSON.parse(fs.readFileSync(path.join(__dirname,"../test_data/samplePosts.json")));
 
-//    var conn = axios.create({
-//     baseURL:'localhost',
-//     proxy:false,
-//     port:5000
-//    });
-
   axios.post('http://localhost:5000/api/buffer/postData',{
     body:sampleData
    });
